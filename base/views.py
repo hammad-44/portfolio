585c56ac70986bb82a2ab6f7f1e62cdc14be232e
Hammad  Haider <105510746+hammad-44@users.noreply.github.com>
2023-10-10 15:36:52 +0500

    Set Blogs to Home

--- base/views.py
+++ base/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from base.models import Contact
 # Create your views here.
+from blog.models import Post, BlogComment
 
 def index(request):
-    return render(request, "home.html")
+    allPosts= Post.objects.all()
+    context={'allPosts': allPosts}
+    return render(request, "home.html",context)
 
 def contact(request):
     if request.method == "POST":
@@ -11,8 +14,8 @@ def contact(request):
         email = request.POST.get("email")
         subject = request.POST.get("subject")
         message = request.POST.get("message")
-        #msg = Contact(name=name,email=email,subject=subject,message=message)
-        #msg.save()
+        msg = Contact(name=name,email=email,subject=subject,message=message)
+        msg.save()
         redirect(f"https://api.whatsapp.com/send?phone=923104416475&text=Name%20{name}%20%\n Email%20{email}%20%\n Subject %20{subject} \n Message %20{message}")
         return render(request, "home.html")
 
